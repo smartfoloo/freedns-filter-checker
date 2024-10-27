32336abbb0c87fe127832557315d2ad0f0412004
smxrtfoloo <smartfoloo@gmail.com>
2024-10-26 19:58:47 -0500

    added Lightspeed API

--- server.js
+++ server.js
@@ -8,6 +8,37 @@ app.use(express.json());
 
 let progress = { completed: 0, total: 0 };
 
+const unblockedCategoryNumbers = [6, 9, 10, 14, 15, 18, 20, 29, 30, 36, 37, 40, 41, 43, 44, 45, 46, 47, 48, 49, 50, 51, 57, 58, 59, 69, 73, 75, 76, 77, 79, 83, 84, 85, 99, 129, 131, 132, 139, 140, 900];
+
+async function fetchCategorization(url) {
+  try {
+    const response = await fetch("https://production-archive-proxy-api.lightspeedsystems.com/archiveproxy", {
+      method: "POST",
+      headers: {
+        'accept': 'application/json, text/plain, */*',
+        'accept-language': 'en-US,en;q=0.9',
+        'authority': 'production-archive-proxy-api.lightspeedsystems.com',
+        'content-type': 'application/json',
+        'origin': 'https://archive.lightspeedsystems.com',
+        'user-agent': 'Mozilla/5.0',
+        'x-api-key': 'onEkoztnFpTi3VG7XQEq6skQWN3aFm3h'
+      },
+      body: `{"query":"\\nquery getDeviceCategorization($itemA: CustomHostLookupInput!, $itemB: CustomHostLookupInput!){\\n  a: custom_HostLookup(item: $itemA) { cat}\\n  b: custom_HostLookup(item: $itemB) { cat   \\n  }\\n}","variables":{"itemA":{"hostname":"${url}"}, "itemB":{"hostname":"${url}"}}}`
+    });
+
+    if (!response.ok) {
+      console.error('Network response was not ok:', response.statusText);
+      return;
+    }
+
+    const body = await response.json();
+    const categories = [body.data.a.cat, body.data.b.cat];
+    return categories;
+  } catch (error) {
+    console.error('Fetch error:', error);
+  }
+}
+
 app.post('/check-links', async (req, res) => {
   const { urls } = req.body;
   progress.completed = 0;
@@ -15,24 +46,23 @@ app.post('/check-links', async (req, res) => {
 
   const results = { unblocked: [] };
 
-  await Promise.all(urls.map(async (url) => {
+  for (const url of urls) {
     const cleanUrl = url.replace(/https?:\/\//, '').replace(/\/$/, '');
-    const apiUrl = `https://filterchecker.rare1k.dev/check/${cleanUrl}/results.txt`;
 
     try {
-      const response = await fetch(apiUrl);
-      const text = await response.text();
+      const categories = await fetchCategorization(cleanUrl);
 
-      const lsFilterMatch = text.match(/LS Filter:.*(Likely \*\*Unblocked\*\*)/);
-      if (lsFilterMatch) {
+      const isUnblocked = categories.some(cat => unblockedCategoryNumbers.includes(cat));
+
+      if (isUnblocked) {
         results.unblocked.push(cleanUrl);
       }
     } catch (error) {
-      console.error(`Error fetching data for ${url}:`, error);
+      console.error(`Error fetching data for ${cleanUrl}:`, error);
     } finally {
       progress.completed++;
     }
-  }));
+  }
 
   res.json(results);
 });
